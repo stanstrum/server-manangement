db2183af411ffcdc9c8e9c9fe108064176dfe22f
Stan Strum <stanleystrum@gmail.com>
2024-04-25 01:40:19 -0700

    misc fixes

--- src/gmcapi/GmcServer.hpp
+++ src/gmcapi/GmcServer.hpp
@@ -9,20 +9,20 @@
 
 struct GmcServerStatus {
   std::string server_ipv4_address; // "208.103.169.107"
-  uint8_t network_usage; // 0
+  uint32_t network_usage; // 0
   uint32_t max_players; // 32
-  uint8_t ram_usage; // 2
+  uint32_t ram_usage; // 2
   std::string serv_name; // "Universe Sandbox [Need Staff] |Wiremod|B"
-  uint8_t workshoperror; // 0
-  uint8_t workshop_progress; // 0
+  uint32_t workshoperror; // 0
+  uint32_t workshop_progress; // 0
   std::string fps; // ""
   std::string ent_cnt; // ""
   uint32_t workshopstatus; // 0
   uint64_t pid; // 28564
   uint32_t active_players; // 0
   uint32_t server_status; // 1
-  uint8_t cpu_usage; // 4
-  uint16_t server_gameport; // "27016"
+  uint32_t cpu_usage; // 4
+  std::string server_gameport; // "27016"
 };
 
 class GmcServer {
